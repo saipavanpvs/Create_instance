e8a3aba55d6a3fa083c38eeaa458bf6d23f957ce
Venkata Sai Pavan Kumar Pothuri <vepo0622@netcracker.com>
2024-02-11 16:37:08 +0530

    auto

--- Launch_instance.sh
+++ Launch_instance.sh
@@ -13,7 +13,7 @@ fi
 
 # AMI_ID="ami-0c1d144c8fdd8d690"
 AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')"
-SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=MY_SECURITY_GROUP | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 
 create_ec2() {
 
